087f5c1d9ad6c487ec3fd2fe596a94033e73177f
Thomas Voth <tvoth@calicosol.com>
2024-09-20 09:53:05 -0600

    Changes made trying to run the code on Tom's system using Visual Studio

--- CalPEATS.ConsoleSandbox/Program.cs
+++ CalPEATS.ConsoleSandbox/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CalPEATS.EfCore.Sandbox;
+
 Console.WriteLine("Hello, World!");
+UserDbContext context = new UserDbContext();
+var version = context.Versions.OrderByDescending(v => v.Build).FirstOrDefault();
+Console.WriteLine(version);
